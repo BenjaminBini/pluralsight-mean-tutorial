4f2ad5e3c4df8133803382a53a0e8bb43762263c
BenjaminBini <benjamin.bini@gmail.com>
2015-02-09 15:09:17 +0100

    Clear notifications when a new one appears

--- public/app/common/mvNotifier.js
+++ public/app/common/mvNotifier.js
@@ -1,16 +1,20 @@
 angular.module('app').value('mvToastr', toastr);
 
 angular.module('app').factory('mvNotifier', function(mvToastr) {
+	toastr.options.progressBar = true; 
 	return {
 		notify: function (msg) {
+			toastr.clear();
 			mvToastr.success(msg);
 			console.log(msg);
 		},
 		warn: function (msg) {
+			toastr.clear();
 			mvToastr.warning(msg);
 			console.log(msg);
 		},
 		error: function (msg) {
+			toastr.clear();
 			mvToastr.error(msg);
 			console.log(msg);
 		}
